80d0523c523469eb13f33c25105847a380e56a94
sarvesh-03 <sarveshjan18@gmail.com>
2022-04-17 22:31:44 +0530

    bug fix

--- turing.cpp
+++ turing.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <map> 
+#include <bits/stdc++.h>
 using namespace std;
 
 enum class Direction{
@@ -19,53 +20,62 @@ struct Rule{
         outputSymbol = output;
         this->direction = direction;
     }
+
+    bool const operator<(const Rule &o) const {
+        return inputSymbol < o.inputSymbol;
+    }
 };
 
 struct State{
     string name;
-    map<Rule, State> transition;
-    State(string name){
-        this->name = name;
-    }
+    map<Rule,State *> transition;
 };
 
 struct TuringMachine
 {
-    State q0, qa, qr;
+    State q0, qa, qr,currentState;
     vector<State> states;
     vector<char> input;
     vector<char> tape;
 
-    TuringMachine();
+    TuringMachine(State q0, State qa, State qr, vector<State> states, vector<char> input, vector<char> tape){
+        this->q0 = q0;
+        this->qa = qa;
+        this->qr = qr;
+        this->states = states;
+        this->input = input;
+        this->tape = tape;
+        currentState =q0;
+    }
 
     Result processInputString(string inputString){
         size_t currentIndex = 0;
-        State currentState = q0;
         Result result = Result::ONGOING;
-        printCurrentConfig(inputString, currentIndex, currentState);
         while(result == Result::ONGOING){
-            handleCurrentChar(inputString, currentIndex, currentState);
-            printCurrentConfig(inputString, currentIndex, currentState);
-            if(currentState.name == qa.name) result = Result::ACCEPTED;
+            handleCurrentChar(&inputString, &currentIndex);            
+            printCurrentConfig(inputString, currentIndex);
+            if(currentState.name == qa.name) 
+                result = Result::ACCEPTED;
             else if(currentState.name == qr.name) result = Result::REJECTED;
         }
         return result;
     }
 
-    void handleCurrentChar(string &inputString, size_t &currentIndex, State &currentState){
-        char &head_symbol = inputString[currentIndex];
-        for(pair<Rule,State> transition : currentState.transition){
-            if(transition.first.inputSymbol == head_symbol){
-                head_symbol = transition.first.outputSymbol;
-                currentIndex = (transition.first.direction == Direction::LEFT)? 
-                            currentIndex -= 1 : currentIndex+=1;
-                currentState = transition.second;
+    void handleCurrentChar(string *inputString, size_t *currentIndex){
+        char &head_symbol = (*inputString)[*currentIndex];
+        for(pair<Rule,State *> trans : currentState.transition){
+            if(trans.first.inputSymbol == head_symbol){
+                head_symbol = trans.first.outputSymbol;
+                *currentIndex = (trans.first.direction == Direction::LEFT)? 
+                            *currentIndex -1 : *currentIndex+1;
+                currentState.name = (*(trans.second)).name;
+                currentState.transition = (*(trans.second)).transition;
                 return;
             }
         }
     }
 
-    void printCurrentConfig(string inputString, size_t currentIndex, State currentState){
+    void printCurrentConfig(string inputString, size_t currentIndex){
         string s1 = "", s2 = "";
         s1.append(inputString, 0, currentIndex);
         s2.append(inputString, currentIndex, inputString.length()-currentIndex);
@@ -78,39 +88,32 @@ int main(){
     Rule r2('1', 'B', Direction::RIGHT);
     Rule r3('B', 'B', Direction::RIGHT);
     
-    State q0("q0");
-    State q1("q1");
-    State q2("q2");
-    State qa("qa");
-    State qr("qr");
+    State q0; q0.name = "q0";
+    State q1; q1.name = "q1";
+    State q2; q2.name = "q2";
+    State qa; qa.name = "qa";
+    State qr; qr.name = "qr";
+   
+    q0.transition.emplace(r1, &q1);
+    q0.transition.emplace(r2, &qr);
     
-    q0.transition.emplace(r1, q1);
-    q0.transition.emplace(r2, qr);
-    
-    q1.transition.emplace(r1, q2);
-    q1.transition.emplace(r2, q1);
-    q1.transition.emplace(r3, qr);
+    q1.transition.emplace(r1, &q2);
+    q1.transition.emplace(r2, &q1);
+    q1.transition.emplace(r3, &qr);
 
-    q2.transition.emplace(r3, qa);
-    q2.transition.emplace(r1, qr);
-    q2.transition.emplace(r2, qr);
+    q2.transition.emplace(r3, &qa);
+    q2.transition.emplace(r1, &qr);
+    q2.transition.emplace(r2, &qr);
 
-    TuringMachine tm;
-    tm.q0 = q0;
-    tm.qa = qa;
-    tm.qr = qr;
-    tm.states = {q0, q1, q2, qa, qr};
-    //Input symbols are for overwriting tape symbols
-    tm.input = {'0', '1'};
-    tm.tape = {'0', '1', 'B'};
+    TuringMachine tm(q0, qa, qr, {q0, q1, q2, qa, qr}, {'0', '1'}, {'0', '1', 'B'});
     
-    //cout << "Enter the input string: ";
-    // string input;
-    // cin >> input;
+    // //cout << "Enter the input string: ";
+    // // string input;
+    // // cin >> input;
     Result result = tm.processInputString("01111110BBBB");
     if(result == Result::ACCEPTED)
         cout << "The input string is accepted." << endl;
-    else
+    else if(result == Result::REJECTED)
         cout << "The input string is rejected." << endl;
 
     return 0;
